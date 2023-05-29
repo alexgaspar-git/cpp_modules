98f4f5a5978094ff374d93d3b1085e8463718404
Alex G <agasparr98@gmail.com>
2023-05-29 23:52:23 +0200

    fini?

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -8,5 +8,21 @@
 #include <deque>
 #include <vector>
 
+#define THRESHOLD 10
+
+template<typename T>
+void merge(typename T::iterator left, typename T::iterator mid, typename T::iterator right);
+
+template<typename T>
+void mergeSort(T &c, typename T::iterator left, typename T::iterator right);
+
+template <typename T>
+void insertionSort(T &c, size_t size);
+
+template <typename T>
+double sort(T &c);
+
+template<typename T>
+void printContainer(T &c);
 
 #endif
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -1,167 +1,133 @@
 #include "PmergeMe.hpp"
 
+template<typename T>
+void merge(typename T::iterator left, typename T::iterator mid, typename T::iterator right) {
+    T temp(left, right + 1);
+    typename T::iterator tempIt = temp.begin();
+    typename T::iterator leftIt = left;
+    typename T::iterator rightIt = mid + 1;
 
-// Function to merge two sorted subvectors into one sorted vector
-void merge(std::vector<int>& arr, int left, int mid, int right) {
-    int i, j, k;
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-
-    // Create temporary vectors
-    std::vector<int> L;
-    std::vector<int> R;
-
-    // Copy data to temporary vectors L[] and R[]
-    for (i = 0; i < n1; i++)
-        L.push_back(arr[left + i]);
-    for (j = 0; j < n2; j++)
-        R.push_back(arr[mid + 1 + j]);
-
-    // Merge the temporary vectors back into arr[left..right]
-    i = 0;
-    j = 0;
-    k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
+    while (leftIt <= mid && rightIt <= right) {
+        if (*leftIt <= *rightIt) {
+            *tempIt = *leftIt;
+            ++leftIt;
         } else {
-            arr[k] = R[j];
-            j++;
+            *tempIt = *rightIt;
+            ++rightIt;
         }
-        k++;
+        ++tempIt;
     }
 
-    // Copy the remaining elements of L[], if there are any
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
+    while (leftIt <= mid) {
+        *tempIt = *leftIt;
+        ++leftIt;
+        ++tempIt;
     }
 
-    // Copy the remaining elements of R[], if there are any
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
+    while (rightIt <= right) {
+        *tempIt = *rightIt;
+        ++rightIt;
+        ++tempIt;
     }
+
+    std::copy(temp.begin(), temp.end(), left);
 }
 
-// Recursive function to perform merge sort on the vector
-void mergeSort(std::vector<int>& arr, int left, int right) {
+template<typename T>
+void mergeSort(T &c, typename T::iterator left, typename T::iterator right) {
     if (left < right) {
-        // Find the middle point
-        int mid = (left + right) / 2;
-
-        // Recursive calls to mergeSort for the left and right halves
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        // Merge the sorted halves
-        merge(arr, left, mid, right);
+        typename T::iterator mid = left + (right - left) / 2;
+        mergeSort(c, left, mid);
+        mergeSort(c, mid + 1, right);
+        merge<T>(left, mid, right);
     }
 }
 
-// Utility function to print the vector
-void printVector(const std::vector<int>& arr) {
-    for (std::vector<int>::size_type i = 0; i < arr.size(); i++)
-        std::cout << arr[i] << " ";
+template <typename T>
+void insertionSort(T &c, size_t size) {
+    size_t i, j;
+    for (i = 1; i < size; i++) {
+        j = i;
+        while (j > 0 && c[j - 1] > c[j]) {
+            std::swap(c[j], c[j - 1]);
+            j = j - 1;
+        }
+    }
+}
+
+template <typename T>
+double sort(T &c) {
+    std::clock_t start = std::clock();
+
+    size_t size = c.size();
+
+    if (size <= THRESHOLD) {
+        insertionSort(c, size);
+    } else {
+        mergeSort(c, c.begin(), c.end() - 1);
+    }
+
+    std::clock_t end = std::clock();
+
+    double time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
+
+    return time;
+}
+
+template<typename T>
+void printContainer(T &c) {
+    typename T::const_iterator it;
+    for (it = c.begin(); it != c.end(); ++it)
+        std::cout << *it << " ";
     std::cout << std::endl;
+}
+
+static bool parseNumbers(char **argv) {
+    for (int i = 1; argv[i]; i++) {
+        char *end;
+        long number = std::strtol(argv[i], &end, 10);
+        if (*end != '\0' || number < 0 || number > INT_MAX) {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main(int argc, char **argv) {
 
-    (void)argc;
-
-    std::vector<int> test;
-
-    for (int i = 1; argv[i]; i++) {
-        test.push_back(atoi(argv[i]));
+    if (argc < 2) {
+        std::cout << "Not enough numbers to sort." << std::endl;
+        return 1;
     }
-    
-    std::cout << "Original array: ";
-    printVector(test);
 
-    // Sort the array using merge sort
-    mergeSort(test, 0, test.size() - 1);
+    if (!parseNumbers(argv)) {
+        std::cout << "Invalid input detected." << std::endl;
+        return 2;
+    }
 
-    std::cout << "Sorted array: ";
-    printVector(test);
+    std::vector<int> vec;
+    for (int i = 1; argv[i]; i++) {
+        vec.push_back(atoi(argv[i]));
+    }
+
+    std::deque<int> deq;
+    for (int i = 1; argv[i]; i++) {
+        deq.push_back(atoi(argv[i]));
+    }
+
+    std::cout << "Before: ";
+
+    printContainer(vec);
+
+    double vecTime = sort(vec);
+    double deqTime = sort(deq);
+
+    std::cout << "After: ";
+
+    printContainer(vec);
+
+    std::cout << "Time to process a range of " << vec.size() << " elements with std::vector : " << vecTime << "ms" << std::endl;
+    std::cout << "Time to process a range of " << deq.size() << " elements with std::deque : " << deqTime << "ms" << std::endl;
 
     return 0;
 }
-
-
-// template <typename T>
-// void insertionSort(T &c, size_t size) {
-//     size_t i, j;
-//     for (i = 1; i < size; i++) {
-//         j = i;
-//         while (j > 0 && c[j - 1] > c[j]) {
-//             std::swap(c[j], c[j - 1]);
-//             j = j - 1;
-//         }
-//     }
-// }
-
-// bool parseNumbers(char **argv) {
-//     for (int i = 1; argv[i]; i++) {
-//         char *end;
-//         long number = std::strtol(argv[i], &end, 10);
-//         if (*end != '\0' || number < 0 || number > INT_MAX) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
-// void printVector(const std::vector<int>& vec) {
-//     for (std::vector<int>::size_type i = 0; i < vec.size(); i++) {
-//         std::cout << vec[i] << " ";
-//     }
-//     std::cout << std::endl;
-// }
-
-// int main(int argc, char **argv) {
-//     if (argc < 2) {
-//         std::cout << "Not enough numbers to sort." << std::endl;
-//         return 1;
-//     }
-
-//     if (!parseNumbers(argv)) {
-//         std::cout << "Invalid input detected." << std::endl;
-//     }
-
-    // std::vector<int> test;
-
-    // for (int i = 1; argv[i]; i++) {
-    //     test.push_back(atoi(argv[i]));
-    // }
-
-//     std::cout << "Before: ";
-//     printVector(test);
-
-//     // insertionSort(test, test.size());
-//     size_t size = sizeof(test) / sizeof(test[0]);
-//     mergeSort(test, 0, size - 1);
-
-//     std::cout << "After: ";
-//     printVector(test);
-
-//     return 0;
-// }
-
-// int main() {
-
-//     std::clock_t start = std::clock();
-
-//     randomFunction();
-
-//     std::clock_t end = std::clock();
-
-//     double timeinMs = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000000;
-
-//     std::cout << "Elapsed time: " << timeinMs << " microseconds." << std::endl;
-
-//     return 0;
-// }
